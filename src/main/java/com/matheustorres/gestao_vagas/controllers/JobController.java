4dfb0519edf8131f1fbc9fc724127ec576d5d596
matheustorresdev97 <matheustorresdev@gmail.com>
2025-03-09 20:23:43 -0300

    refatorando JobController

--- src/main/java/com/matheustorres/gestao_vagas/controllers/JobController.java
+++ src/main/java/com/matheustorres/gestao_vagas/controllers/JobController.java
@@ -1,5 +1,7 @@
 package com.matheustorres.gestao_vagas.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheustorres.gestao_vagas.models.JobModel;
+import com.matheustorres.gestao_vagas.records.CreateJobRecord;
 import com.matheustorres.gestao_vagas.services.JobService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -20,9 +24,19 @@ public class JobController {
     private JobService jobService;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@Valid @RequestBody JobModel jobModel) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobRecord createJobRecord,
+            HttpServletRequest request) {
         try {
-            var result = this.jobService.save(jobModel);
+            var companyId = request.getAttribute("company_id");
+
+            var jobEntity = JobModel.builder()
+                    .benefits(createJobRecord.benefits())
+                    .companyId(UUID.fromString(companyId.toString()))
+                    .description(createJobRecord.description())
+                    .level(createJobRecord.level())
+                    .build();
+
+            var result = this.jobService.save(jobEntity);
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
---
+++ src/main/java/com/matheustorres/gestao_vagas/records/CreateJobRecord.java
@@ -0,0 +1,5 @@
+package com.matheustorres.gestao_vagas.records;
+
+public record CreateJobRecord(String description, String benefits, String level) {
+
+}
