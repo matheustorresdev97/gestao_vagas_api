88d1820f52fa6f94a5148e9f2ccdd609e8bc0078
matheustorresdev97 <matheustorresdev@gmail.com>
2025-03-09 20:24:50 -0300

    hash da senha do candidato

--- src/main/java/com/matheustorres/gestao_vagas/services/CandidateService.java
+++ src/main/java/com/matheustorres/gestao_vagas/services/CandidateService.java
@@ -1,6 +1,7 @@
 package com.matheustorres.gestao_vagas.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.matheustorres.gestao_vagas.exceptions.UserFoundException;
@@ -13,11 +14,17 @@ public class CandidateService {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateModel save(CandidateModel candidateModel) {
         this.candidateRepository.findByUsernameOrEmail(candidateModel.getUsername(),
                 candidateModel.getEmail()).ifPresent(user -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateModel.getPassword());
+        candidateModel.setPassword(password);
 
         return this.candidateRepository.save(candidateModel);
     }
